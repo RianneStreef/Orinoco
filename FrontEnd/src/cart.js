7db88531c1f53b89f219efdd75f2c4d879ffe30e
RianneStreef <riannestreef@gmail.com>
2020-11-06 18:38:16 +0100

    tries with remove item

--- FrontEnd/src/cart.js
+++ FrontEnd/src/cart.js
@@ -28,7 +28,7 @@ async function productsInCart() {
 const cameraDisplay = document.getElementById("selectedItems");
 
 async function displayProductsInCart() {
-  const productsToDisplay = await productsInCart();
+  let productsToDisplay = await productsInCart();
   if (productsToDisplay == null){
     //do nothing
   }
@@ -37,6 +37,7 @@ async function displayProductsInCart() {
     const name = camera.name;
     const price = camera.price;
     const image = camera.imageUrl;
+    const id = camera._id;
   
       const cardContainer = document.createElement('div');
         cardContainer.className = "card";
@@ -50,11 +51,11 @@ async function displayProductsInCart() {
       const button = document.createElement('button')
         button.className = "button";
         button.onclick = function(){
-          removeItem();
-          window.location.reload();    
+          console.log('current id ' + id);
+          removeItem(id);
+          // window.location.reload();    
         };
-        // loop trough array to find index of this id zo it can be removed
-        // localStorage removeItem[1];
+
 
       textContainer.classList.add('cart-card__text');
       nameContainer.classList.add('cart-card__name');
@@ -83,6 +84,97 @@ async function displayProductsInCart() {
 
 displayProductsInCart();
 
+// onclick, use productsInCart function to get all products, compare to the 
+//current id and is it is not the same, add it to the new products to display
+// list, and display that list
+
+// function removeItem(){
+//   productsToDisplay = newProductsToDisplay;
+//   displayProductsInCart();
+// };
+
+
+// function newProductsToDisplay() {
+// let productsInCart = productsInCart();
+// const newProductsToDisplay = productsInCart.filter((currentProduct) => {
+//   for (let i = 0; i < productsInCart.length; i++) {
+//     if (productsInCart[i] !== currentProduct._id) {
+//       console.log(newProductsToDisplay);
+//       return true; 
+//   }
+//   }
+// });
+// }
+
+// or, just get this one out of the localStorage, and re-call the displayProductsInCart function
+
+
+
+async function displayNewProductsInCart() {
+let newProductsToDisplay = removeItem();
+  for (let camera of newProductsToDisplay) {
+    const name = camera.name;
+    const price = camera.price;
+    const image = camera.imageUrl;
+    const id = camera._id;
+  
+      const cardContainer = document.createElement('div');
+        cardContainer.className = "card";
+        cardContainer.classList.add('cart-card');
+
+      const textContainer = document.createElement('div');
+      const nameContainer = document.createElement('div');
+      const priceContainer = document.createElement('div');
+      const imgContainer = document.createElement('img');   
+      const buttonContainer = document.createElement('div')
+      const button = document.createElement('button')
+        button.className = "button";
+        button.onclick = function(){
+          removeItem(id);
+          // window.location.reload();    
+        };
+
+      textContainer.classList.add('cart-card__text');
+      nameContainer.classList.add('cart-card__name');
+      priceContainer.classList.add('cart-card__price');
+      imgContainer.classList.add('cart-card__img');
+      buttonContainer.classList.add('cart-container');
+      button.classList.add('cart-card__button');
+  
+      nameContainer.innerText = name;
+      priceContainer.innerText = price;
+      imgContainer.src = image;
+      button.innerText = 'Remove';
+
+
+      cardContainer.appendChild(imgContainer);
+      cardContainer.appendChild(nameContainer);
+      cardContainer.appendChild(priceContainer);
+      cardContainer.appendChild(buttonContainer);
+
+      buttonContainer.appendChild(button);
+  
+      cameraDisplay.appendChild(cardContainer);
+  }
+}
+
+function removeItem(id){
+  const productsInCart = JSON.parse(localStorage.getItem('cameras'));
+  let thisProduct = id;
+  const newProductsToDisplay = productsInCart.filter((thisProduct) => {
+    for (let i = 0; i < productsInCart.length; i++) {
+      if (productsInCart[i] !== thisProduct) {
+        return true;
+      }
+    }
+  });
+  console.log(newProductsToDisplay);
+  displayNewProductsInCart();
+  return newProductsToDisplay;
+}
+
+//opdelen in meerdere functies
+
 function deleteAllItems() {
   localStorage.clear();
   window.location.reload();    
@@ -93,6 +185,8 @@ emptyCartButton.onclick = () => {
   deleteAllItems();
 }
 
+
+
 const totalPrice = document.getElementById('totalPrice');
 
 async function getCamerasToAdd() {
@@ -102,38 +196,25 @@ async function getCamerasToAdd() {
 }
 
 
-let camerasToAdd = getCamerasToAdd()
-  .then(() => {let pricesToAdd = camerasToAdd.map(function (camera) {
-    console.log(pricesToAdd);
-    return camera.price
-  });
-})
+// let camerasToAdd = getCamerasToAdd()
+//   .then((camerasToAdd) => {let pricesToAdd = camerasToAdd.map(function (camera) {
+//     console.log(pricesToAdd);
+//     return price = camera.price
+//   });
+// })
 
-console.log(camerasToAdd);
-
-
-
-// let camerasToAdd = getCamerasToAdd();
 // console.log(camerasToAdd);
 
-
-
-// console.log(pricesToAdd());
-
-// getCamerasToAdd();
-
-
-
-
-//   console.log(pricesToAdd);
-//   let allPricesAdded = pricesToAdd.reduce(function(a, b){
-//     return a + b;
+// function allPricesAdded() {
+// let allPricesAdded = pricesToAdd.reduce(function(a, b){
+//   return a + b;
 //   }, 0);
-//   console.log(allPricesAdded);
-//   return allPricesAdded;
+  
+// console.log(allPricesAdded());
+// return allPricesAdded;
 // }
 
 
-// totalPrice.innerText = allPricesAdded;
+// totalPrice.innerText = allPricesAdded();
 
 
