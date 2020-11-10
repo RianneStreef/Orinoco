b0a53a71897655faf7e69c71866fc169a702c6b5
RianneStreef <riannestreef@gmail.com>
2020-11-10 15:58:43 +0100

    remove one product working

--- FrontEnd/src/cart.js
+++ FrontEnd/src/cart.js
@@ -13,43 +13,36 @@ window.onscroll = function () {
  * @returns {Array} productsToDisplay
  */
 
-const itemsInCart = JSON.parse(localStorage.getItem('cameras'));
+let itemsInCart = JSON.parse(localStorage.getItem('cameras'));
 
 async function productsInCart() {
   if (itemsInCart == null) {
-    //do nothing
-    return
+    return null;
   }
-  else {
   const allProducts = await cameraService.collection;
   const productsToDisplay = allProducts.filter((currentProduct) => {
     for (let i = 0; i < itemsInCart.length; i++) {
       if (itemsInCart[i] === currentProduct._id) {
-        // console.log('items in cart ' + itemsInCart[i]);
-        // console.log("current id " + currentProduct._id); 
-        // why do I not get all the 5 product ids?    
-        return true; 
+        return true;
       }
     }
   });
   return productsToDisplay;
 }
-}
-//what if I loop first and filter then.. 
 
 const cameraDisplay = document.getElementById("selectedItems");
-
 
 /**
  * display products in cart
  * @param {Array} itemsInCart
  */
 async function displayProductsInCart() {
+  itemsInCart = JSON.parse(localStorage.getItem('cameras'));
   let productsToDisplay = await productsInCart();
   if (productsToDisplay == null){
-    //do nothing
+    return null;
   }
-  else {
+    cameraDisplay.innerText = '';
   for (let camera of productsToDisplay) {
     const name = camera.name;
     const price = camera.price;
@@ -67,13 +60,7 @@ async function displayProductsInCart() {
       const buttonContainer = document.createElement('div')
       const button = document.createElement('button')
         button.className = "button";
-        button.onclick = function(){
-          removeItem(id);
-          console.log(id);
-          // window.location.reload();    
-        };
-
-
+        button.onclick = () => removeItem(id, itemsInCart);
       textContainer.classList.add('cart-card__text');
       nameContainer.classList.add('cart-card__name');
       priceContainer.classList.add('cart-card__price');
@@ -95,8 +82,51 @@ async function displayProductsInCart() {
       buttonContainer.appendChild(button);
   
       cameraDisplay.appendChild(cardContainer);
+
+      async function getCamerasToAdd() {
+        let camerasToAdd = await productsInCart();
+        if (camerasToAdd == null) {
+          return null;
+        }
+  
+        let pricesToAdd = camerasToAdd.map(function (camera) {
+          return camera.price;
+          });
+          return pricesToAdd;
+          
+        }
+
+      async function allPricesAdded() {
+        let pricesToAdd = await getCamerasToAdd();
+        if (pricesToAdd == null) {
+          return null;
+        }
+        let allPricesAdded = pricesToAdd.reduce(function(a, b){
+          return a + b;
+          }, 0);
+          
+        return allPricesAdded;
+          
+        }
+        const totalPrice = document.getElementById('totalPrice');
+        
+        
+        /**
+         * Displays the total price on site
+         * @param {Number} price
+         */
+        async function total() {
+         let price = await allPricesAdded();
+         if (price == null) {
+         return null;
+          
+         }
+         else {
+        totalPrice.innerText = price;
+        }
+        }
+        total();     
   }
-}
 }
 
 displayProductsInCart();
@@ -105,43 +135,15 @@ displayProductsInCart();
 /**
  * Get localStorage to check against selected Id 
  * @param {*} id 
- * @returns {Array} New list of items to display
+ * @returns {Array} New list of items to put in localStorage
  */
 
-function removeItem(id){
-  let productsToDisplay = newProductsToDisplay(id);
-  console.log('products to display after removal ' + productsToDisplay);
-  localStorage.setItem('cameras', JSON.stringify(productsToDisplay));
-  // window.location.reload();    
-}
-
-/**
- *  remove one selected item from cart by looping trough items in cart, and comparing
- * those to the selected Id
- * @param {*} id 
- * @returns {Array} List of items to put in localStorage
- */
-
-function newProductsToDisplay(id) {
-let newProductsToDisplay = itemsInCart.filter(() => {
-  for (let i = 0; i < itemsInCart.length; i++) {
-    console.log('items in cart ' + itemsInCart[i]);
-    console.log("current id " + id);    
-
-    if (itemsInCart[i] !== id) {
-      return true; 
-      }
-    }
-});
-
-if (newProductsToDisplay == null) {
-  console.log("cart empty")
-  return [];
-  }
-else {
-  // window.location.reload();    
-  return newProductsToDisplay;
-  }
+function removeItem(id, itemsArr) {
+  console.log('removeItem');
+  const newCollection = itemsArr.filter((item) => item !== id);
+  console.log(newCollection);
+  localStorage.setItem('cameras', JSON.stringify(newCollection));
+  displayProductsInCart();
 }
 
 /**
@@ -158,66 +160,7 @@ emptyCartButton.onclick = () => {
   deleteAllItems();
 }
 
-/**
- * Gets items in cart, and puts the prices of those items in an Array to be 
- * added to a total price
- * @param {Array} camerasToAdd
- * @returns {Array} pricesToAdd
- */
 
-async function getCamerasToAdd() {
-let camerasToAdd = await productsInCart();
-if (camerasToAdd == null) {
-  // do nothing
-}
-else {
-let pricesToAdd = camerasToAdd.map(function (camera) {
-  return camera.price;
-  });
-  return pricesToAdd;
-  }
-}
-
-getCamerasToAdd();
-
-/**
- * Add up all the prices of items in cart to a total price
- * @param {Array} pricesToAdd
- * @returns {Number} total price
- */
-
-async function allPricesAdded() {
-let pricesToAdd = await getCamerasToAdd();
-if (pricesToAdd == null) {
-  // do nothing
-}
-else {
-let allPricesAdded = pricesToAdd.reduce(function(a, b){
-  return a + b;
-  }, 0);
-  
-return allPricesAdded;
-  }
-}
-const totalPrice = document.getElementById('totalPrice');
-
-
-/**
- * Displays the total price on site
- * @param {Number} price
- */
-async function total() {
- let price = await allPricesAdded();
- if (price == null) {
-
-//do nothing
- }
- else {
-totalPrice.innerText = price;
-}
-}
-
-total();
 
 
 /**
@@ -251,22 +194,3 @@ let contact = createContact();
 
 
 
-const formData = new FormData();
-
-// formData.append('product Array', itemsInCart);
-// formData.append('contact Object', contact);
-
-
-// this works with GET, not POST
-fetch("http://localhost:3000/api/cameras", {
-  method: 'POST',
-//   body: JSON.stringify(contact),
-//   body: JSON.stringify(itemsInCart),
-})
-.then(response => response.json())
-.then(data => {
-  console.log("success:", data);
-})
-.catch((error) => {
-  console.error("error:", error);
-});
